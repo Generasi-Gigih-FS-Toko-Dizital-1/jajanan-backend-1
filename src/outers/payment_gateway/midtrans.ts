e6a167c2b6f8e0c3ad7b88d13735877997d1e53d
ridhoafwani <zikrurridhoafwani@gmail.com>
2023-09-30 20:31:59 +0800

    JJ-39 Added Midtrans Credentials

--- src/outers/payment_gateway/midtrans.ts
+++ src/outers/payment_gateway/midtrans.ts
@@ -2,6 +2,6 @@ import midtransClient from 'midtrans-client'
 
 export const snap = new midtransClient.Snap({
   isProduction: false,
-  serverKey: '',
-  clientKey: ''
+  serverKey: process.env.MIDTRANS_SERVER_KEY,
+  clientKey: process.env.MIDTRANS_CLIENT_KEY
 })
