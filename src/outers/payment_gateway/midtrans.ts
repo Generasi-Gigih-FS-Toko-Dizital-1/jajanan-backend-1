336da212c382c258a2de7faaeac7732f015962e2
ridhoafwani <zikrurridhoafwani@gmail.com>
2023-09-30 20:36:45 +0800

    jj-39 use export default

--- src/outers/payment_gateway/midtrans.ts
+++ src/outers/payment_gateway/midtrans.ts
@@ -1,7 +1,9 @@
 import midtransClient from 'midtrans-client'
 
-export const snap = new midtransClient.Snap({
+const snap = new midtransClient.Snap({
   isProduction: false,
   serverKey: process.env.MIDTRANS_SERVER_KEY,
   clientKey: process.env.MIDTRANS_CLIENT_KEY
 })
+
+export default snap
