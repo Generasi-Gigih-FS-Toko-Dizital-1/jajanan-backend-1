1a6eb212c62abf6f2ac66b25fd488a7b3ef5bded
ridhoafwani <zikrurridhoafwani@gmail.com>
2023-10-04 11:31:04 +0800

    JJ-41 use class on midtrans.ts

--- src/outers/payment_gateway/midtrans.ts
+++ src/outers/payment_gateway/midtrans.ts
@@ -1,9 +1,13 @@
 import midtransClient from 'midtrans-client'
 
-const snap = new midtransClient.Snap({
-  isProduction: false,
-  serverKey: process.env.MIDTRANS_SERVER_KEY,
-  clientKey: process.env.MIDTRANS_CLIENT_KEY
-})
+export default class Midtrans {
+  snap
 
-export default snap
+  constructor () {
+    this.snap = new midtransClient.Snap({
+      isProduction: false,
+      serverKey: process.env.MIDTRANS_SERVER_KEY,
+      clientKey: process.env.MIDTRANS_CLIENT_KEY
+    })
+  }
+}
