d9faf710900cbff4f5f48e1289a13ef6846e5bdb
Muhammad Kharisma Azhari <kharisma.azhari02@gmail.com>
2023-11-10 18:25:00 +0700

    JJ-76 Rename file feature.

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # jajanan-backend-1 
+ 
+## Description
+This is a backend for jajanan app. This app is a simple app for ordering food. This app has 3 roles, admin, vendor, and user. Admin can manage all data, vendor can manage its product, and customer can order product.   
+ 
+## Features 
+ 
+### Managements 
+- Admin Management
+- Category Management
+- Jajan Item Management
+- Jajan Item Snapshot Management
+- Payout History Management
+- Session Management
+- Top Up Management
+- Transaction History Management
+- Transaction Item History Management
+- User Level Management
+- User Management
+- User Subscription Management
+- Vendor Level Management
+- Vendor Management
+
+### 
+
--- src/outers/routes/RootRoute.ts
+++ src/outers/routes/RootRoute.ts
@@ -71,7 +71,7 @@ import PayoutWebhook from '../../inners/use_cases/payouts/PayoutWebhook'
 import PayoutHistoryManagement from '../../inners/use_cases/managements/PayoutHistoryManagement'
 import PayoutHistoryController from '../controllers/rests/PayoutHistoryControllerRest'
 import CloudinaryGateway from '../gateways/CloudinaryGateway'
-import FileUpload from '../../inners/use_cases/file_uploads/FileUpload'
+import FileManagement from '../../inners/use_cases/managements/FileManagement'
 import CloudinaryUtility from '../utilities/CloudinaryUtility'
 import FileControllerRest from '../controllers/rests/FileControllerRest'
 import TransactionItemHistoryManagement from '../../inners/use_cases/managements/TransactionItemHistoryManagement'
@@ -131,6 +131,7 @@ export default class RootRoute {
     const topUpHistoryManagement: TopUpHistoryManagement = new TopUpHistoryManagement(topUpHistoryRepository, userManagement, objectUtility)
     const userSubscriptionManagement: UserSubscriptionManagement = new UserSubscriptionManagement(userSubscriptionRepository, objectUtility)
     const payoutHistoryManagement: PayoutHistoryManagement = new PayoutHistoryManagement(payoutHistoryRepository, vendorManagement, objectUtility)
+    const fileManagement: FileManagement = new FileManagement(cloudinaryGateway, cloudinaryUtility, objectUtility)
 
     const subscriptionUser: SubscriptionUser = new SubscriptionUser(userSubscriptionManagement)
 
@@ -157,8 +158,6 @@ export default class RootRoute {
     const payout: Payout = new Payout(vendorPayoutRepository, paymentGateway, vendorManagement)
 
     const locationSync: LocationSync = new LocationSync(userManagement, vendorManagement, sessionManagement, firebaseGateway, objectUtility)
-
-    const fileUpload: FileUpload = new FileUpload(cloudinaryGateway, cloudinaryUtility, objectUtility)
 
     const userControllerRest: UserControllerRest = new UserControllerRest(
       Router(),
@@ -292,7 +291,7 @@ export default class RootRoute {
     payoutHistoryControllerRest.registerRoutes()
     routerVersionOne.use('/payout-histories', payoutHistoryControllerRest.router)
 
-    const fileControllerRest: FileControllerRest = new FileControllerRest(Router(), fileUpload, authenticationValidation)
+    const fileControllerRest: FileControllerRest = new FileControllerRest(Router(), fileManagement, authenticationValidation)
     fileControllerRest.registerRoutes()
     routerVersionOne.use('/files', fileControllerRest.router)
 
